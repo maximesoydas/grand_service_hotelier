8ec9e7e7a66982930673c3ea26d1c35f33bf30c1
maximesoydas <max.soydas@gmail.com>
2024-07-16 12:33:08 +0200

    wip(django setup, homepage done, views commented)

---
+++ interventions_web_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InterventionsWebManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'interventions_web_manager'
---
+++ interventions_web_manager/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+from interventions_web_manager.utils import read_csv
+# Create your views here.
+
+
+def index(request):
+    '''
+    Homepage
+    '''
+    return render(request, 'index.html')
+
+def interventions(request):
+    '''
+    list interventions
+    read our database : "Base de données - Interventions Strasbourg - Test recrutement.csv"
+    translate adresses into coordinates
+    populate django template with data from csv file
+    '''
+    return render(request, 'interventions.html')
+
+def update_interventions(request):
+    '''
+    update interventions
+    get request of new data from interventions.html
+    update csv file (database)
+    '''
+    return render(request, 'interventions.html')
+
+
+def backup_interventions_db(request):
+    '''
+    before updating create a copy of the db in its current state
+    filename has date and time as well as version number of database
+    '''
+    
+    
+def list_backup_interventions_db(request):
+    '''
+    list all backups
+    allow download
+    '''
+    return render(request, 'backup_interventions_list.html')
